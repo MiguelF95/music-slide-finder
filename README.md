e3563762899b8d38e06bae4272304149ddbb7c24
miguelf <miguelf@sfu.ca>
2019-11-12 13:18:15 -0800

    Created initial version of script. Refer to README.md for info on usage.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-*Music Slide Finder*
+# Powerpoint Slide Finder
 
-Simple Python script that takes a powerpoint slide and song names as input and returns them as a list with their corresponding slide number.
+Simple Python script that takes powerpoint slide titles as input and returns them as a list with their corresponding slide number.
+
+>**Notes:**
+> - The first PowerPoint file found in the same directory as this script is parsed through. Ensure only the desires .pptx file is in the same folder
+> - Only and exact match with the slide title is returned (case-sensitive and delimiter-sensitive)
+
+>**Dependencies:**
+> - python-pptx (https://github.com/scanny/python-pptx)
+> - glob
---
+++ pptslidefinder.py
@@ -0,0 +1,38 @@
+import glob
+from pptx import Presentation
+
+songlist = {}
+inp = ""
+
+print("\n\nThis script will take in PowerPoint Slide titles and return them with their corresponding slide numbers.\n")
+
+#Obtain Slide titles from user input and add to a dictionary with value -1
+while inp != '1':
+    inp = (input("Input a slide title and press enter to continue adding more titles to the list.\nWhen finished, type '1' and press enter\n\n"))
+    songlist[inp] = -1
+    print("\n\n")
+
+#Remove 1 from songlist
+del songlist['1']
+
+#Get Powerpoint File from current directory (same directory as this script)
+pptname = glob.glob('*.pptx')
+prs = Presentation(pptname[0])
+
+#if a slide's title matches one in the songlist, update the value to the corresponding slide number
+for slide in prs.slides:
+    if slide.shapes.title.text in songlist:
+        songlist[slide.shapes.title.text] = prs.slides.index(slide) + 1
+
+
+#Print slide numbers with their corresponding titles
+#Titles that were not found remain -1 in dicitonary and are not printed
+print("Slide Numbers from " + pptname[0])
+print("=============================================================")
+
+for i in songlist:
+    if songlist[i] != -1:
+         print(  str(songlist[i]) + ' (' + i + ')'  )
+   
+
+print("=============================================================")
