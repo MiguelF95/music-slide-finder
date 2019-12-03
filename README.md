d5be26e9d87826f082bb941f5d0096dc10dafe29
miguelf <miguelf@sfu.ca>
2019-12-03 13:59:18 -0800

    - Now supports pasting all slide titles at once - No longer case sensitive - No longer punctuation sensitive - Prints titles that were not found in the .pptx file

--- README.md
+++ README.md
@@ -4,15 +4,17 @@ Simple Python script that takes powerpoint slide titles as input and returns the
 
 **Notes:**
  - The first PowerPoint file found in the same directory as this script is parsed through. Ensure only the desired .pptx file is in the same folder
- - Only an exact match with the slide title is returned (case-sensitive and delimiter-sensitive)
+ - Only word-for-word match with the slide title is returned (there must be no missing words or spelling mistakes)
+ - Titles are not longer case or punctuation sensitive 
 
 **Dependencies:**
  - python-pptx (https://github.com/scanny/python-pptx)
  - glob
+ - string
 
 **TODO:**
- - Allow user to choose a specific .pptx file
- - Relax slide title match constraints to allow case and delimiter mistakes
+ ~~- Relax slide title match constraints to allow case and delimiter mistakes~~
+ ~~- Return list of slide titles not found~~
+ ~~- Allow user to input all slide titles at once, separated by either newline or comma~~ (Only supports separation by new line)
  - Relax slide title match constraints to allow for spelling mistakes
- - Return list of slide titles not found
- - Allow user to input all slide titles at once, separated by either newline or comma
+ - Allow user to choose a specific .pptx file
--- pptslidefinder.py
+++ pptslidefinder.py
@@ -1,22 +1,28 @@
 import glob
+import string
 from pptx import Presentation
 
-songlist = {}
-inp = ""
+songDict = {}
+inputStr = []
 
 print("\n\nThis script will take in PowerPoint Slide titles and return them with their corresponding slide numbers.\n")
 
 
 #Obtain Slide titles from user input and add to a dictionary with value -1. Input requests stop when user inputs 1
-while inp != '1':
-    inp = (input("Input a slide title and press enter to continue adding more titles to the list.\nWhen finished, type '1' and press enter\n\n"))
-    songlist[inp] = -1
+while '1' not in inputStr:
+    #In the case that user input all titles at the same time via copy and paste, split into separate strings and store into a vector.
+    #Otherwise, each song is put into the vector one at a time.
+    inputStr = (input("Input a slide title and press enter to continue adding more titles to the list.\nWhen finished, type '1' and press enter\n\n")).split("\n")
+    
+    #Create keys using input string, removing punctuation and setting all to lower case.
+    for mystr in inputStr:
+        key = mystr.translate(mystr.maketrans('', '', string.punctuation)).lower().replace(" ", "")
+        songDict[key] = [mystr,-1]
     print("\n\n")
 
 
 #Remove 1 from songlist
-del songlist['1']
-
+del songDict['1']
 
 #Get Powerpoint File from current directory (same directory as this script)
 pptname = glob.glob('*.pptx')
@@ -25,17 +31,23 @@ prs = Presentation(pptname[0])
 
 #if a slide's title matches one in the songlist, update the value to the corresponding slide number
 for slide in prs.slides:
-    if slide.shapes.title.text in songlist:
-        songlist[slide.shapes.title.text] = prs.slides.index(slide) + 1
+    keyCheck = slide.shapes.title.text.translate(str.maketrans('', '', string.punctuation)).lower().replace(" ", "")
+    if keyCheck in songDict:
+        songDict[keyCheck] = [slide.shapes.title.text,
+                              prs.slides.index(slide) + 1]
 
 
 #Print slide numbers with their corresponding titles
-#Titles that were not found remain -1 in dicitonary and are not printed
+#Titles that were not found remain -1 in dictionary and  are printed afterwards as Not Found
 print("Slide Numbers from " + pptname[0])
 print("=============================================================")
 
-for i in songlist:
-    if songlist[i] != -1:
-         print(  str(songlist[i]) + ' (' + i + ')'  )
+for i in songDict:
+    if songDict[i][1] != -1:
+         print(str(songDict[i]))
+
+for i in songDict:
+    if songDict[i][1] == -1:
+         print('Not Found: ' + songDict[i][0])
 
 print("=============================================================")
